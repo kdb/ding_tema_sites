433c81011a4268788db1f64e23011ea027c386d0
Jesper Kristensen <cableman@linuxdev.dk>
2010-12-15 14:07:38 +0100

    Fixed temaheader image issues

--- ding_panels_tema/templates/ding-panels-tema-header.tpl.php
+++ ding_panels_tema/templates/ding-panels-tema-header.tpl.php
@@ -1,5 +1,7 @@
-<div class="picture-inner">
-  <?php print $titleimage ?>
-</div>
+<?php if ($titleimage) : ?>
+  <div class="picture-inner">
+    <?php print $titleimage ?>
+  </div>
+<?php endif; ?>
 <h1><?php print $title ?></h1>
 <?php print $menu; ?>
